08ca4136db6cf5ea49c1e75dc4de4b38925a0ebf
Lucas Hardt <Luc1412.lh@gmail.com>
2020-08-30 21:53:51 +0200

    update cog
    Took 4 minutes

--- update/selection.py
+++ update/selection.py
@@ -130,9 +130,8 @@ class ReactionSelection(SelectionBase):
     def _check(self, reaction, user):
         correct_reaction = False
         for reaction_ in self.reactions:
-            print(reaction_)
-            print(str(reaction_) != str(reaction.emoji))
-            print(str(reaction_) != str(reaction.emoji))
+            print(reaction_, reaction)
+            print(reaction_ != reaction.emoji, str(reaction_) != str(reaction.emoji))
             if str(reaction_) != str(reaction.emoji):
                 continue
             correct_reaction = True
