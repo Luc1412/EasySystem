cb3c7a2542d50672dea355ba8531de264019ef6c
Lucas Hardt <Luc1412.lh@gmail.com>
2020-09-22 01:53:35 +0200

    update cog
    Took 2 minutes

--- commandchannel/commandchannel.py
+++ commandchannel/commandchannel.py
@@ -129,7 +129,9 @@ class CommandChannel(BaseCog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.author.bot or await self.bot.is_automod_immune(message.author):
+        if not isinstance(message.channel, discord.TextChannel) or message.author.bot:
+            return
+        if await self.bot.is_automod_immune(message.author):
             return
         command_channel_ids = await self.settings.guild(message.guild).channel_ids()
         if message.channel.id not in command_channel_ids:
