e74d7ecccdf4c503fd5e0fca98a3426a1d08c297
Lucas Hardt <Luc1412.lh@gmail.com>
2023-08-14 19:31:02 +0200

    Implement premium role case for translators

--- easyfnstats/easyfnstats.py
+++ easyfnstats/easyfnstats.py
@@ -30,11 +30,16 @@ class EasyFnStats(commands.Cog):
 
         guild = self.bot.get_guild(341939185051107330)
         premium_role = guild.get_role(341940409309593606)
+        translator_role = guild.get_role(498924054607298560)
         for user_id in premium_user_ids:
             member = guild.get_member(user_id)
             if not member:
                 continue
             if premium_role not in member.roles:
+                user_data = [e for e in data if e['id'] == user_id][0]
+                # If user has translator role and source is grant, do not grant role
+                if translator_role in member.roles and user_data['source'] == 'grant':
+                    continue
                 await member.add_roles(premium_role)
 
         for member in premium_role.members:
